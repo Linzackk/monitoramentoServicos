b4a572f360fe2ad761b20ffdfe5da2c01d683d9e
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2026-01-06 16:48:20 -0300

    feat procura de serviços usando o BullMQ

--- src/queues/serviceHealth.worker.ts
+++ src/queues/serviceHealth.worker.ts
@@ -1,14 +1,36 @@
 import { Worker } from "bullmq";
 import { redisConfig } from "./redis";
 import { searchAllServicesDb } from "../database/services/services.read";
-
+import axios from "axios";
 
 new Worker(
     "service-health",
     async (job) => {
         const services = await searchAllServicesDb();
 
-        console.log("Serviços: ", services.length)
+        if (services.length === 0) {
+            console.log("Nenhum servico cadastrado")
+            return;
+        }
+
+        const service = services[1];
+
+        console.log("Verificando servico: ", service.name)
+
+        try {
+            const start = Date.now();
+
+            await axios.get(service.url, {
+                timeout: 5000
+            });
+
+            const duration = Date.now() - start;
+
+            console.log("serviço online");
+            console.log("tempo de resposta: ", duration, "ms")
+        } catch (error) {
+            console.log("Serviço offline")
+        }
     },
     {
         connection: redisConfig
