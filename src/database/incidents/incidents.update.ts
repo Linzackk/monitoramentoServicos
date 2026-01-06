58a8592d3c4e549bcfa8f27898a65cacb22f3387
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2026-01-06 15:53:27 -0300

    feat crud de incidents no db

---
+++ src/database/incidents/incidents.update.ts
@@ -0,0 +1,18 @@
+
+import { prisma } from "../../utils/prisma";
+
+export async function updateIncidentDb(incidentId: number, endedAt?: Date, durationSeconds?: number, reason?: string) {
+    try {
+        const updatedIncident = await prisma.incident.update({
+            where: {id: incidentId},
+            data: {
+                ended_at: endedAt,
+                duration_seconds: durationSeconds,
+                reason: reason
+            }
+        });
+        return updatedIncident;
+    } catch (error: any) {
+        throw error;
+    }
+}
