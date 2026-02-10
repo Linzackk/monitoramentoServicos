b2420ad178b6d9bcfd9872e42dc8163f0ec46875
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-02-10 20:36:11 -0300

    feat remocao do dist, node_modules e prisma cache do repo

--- dist/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-const appError_1 = require("../utils/appError");
-const utilNumbers_1 = require("../utils/utilNumbers");
-function notFound(req, res, next) {
-    next(new appError_1.AppError("Rota não encontrada", utilNumbers_1.statusCodes.NOT_FOUND));
-}
