0942cc9206f0befc2c21d16ead98089bd38e289c
Isaac de Medeiros <122842562+Linzackk@users.noreply.github.com>
2026-02-10 20:34:29 -0300

    Delete dist directory

--- dist/queues/redis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConfig = void 0;
-require("dotenv/config");
-exports.redisConfig = {
-    url: process.env.REDIS_URL,
-};
